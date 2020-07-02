9ea85724e0b095094bc8fe6d39e72c6233d9f5c8
Drhellbent <67497916+Drhellbent@users.noreply.github.com>
2020-07-02 13:21:01 +0530

    Index
    Nav

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Medication</title>
+	<meta charset="URF8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Important header tags -->
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	<link href="css/style.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.css">
+</head>
+<body>
+	<header>
+		<nav class="navbar nabbar-expand-lg">
+			<div class="container">
+				<div class="row">
+					
+					<div class="nav-content" style="display:flex;">
+						
+						<div class="nav-item">
+							<ul style="list-style: none; display: flex;">
+								<li style="padding-left: 0px;"><a class="" href="">Home</a></li>
+								<li style="padding-left: 30px;"><a class="" href="">Product</a></li>
+								<li style="padding-left: 30px;"><a class="" href="">Contact</a></li>
+								<li style="padding-left: 30px;"><a class="" href="">About</a></li>
+			
+							</ul>
+						</div>
+					
+						
+							<div class="nav-brand text-center" style="padding-left: 40%;">
+								<img src="images/logo.png" height="70.0938px">
+							</div>
+				
+						
+							<div class="cart" style="display: flex; padding-left: 80%;">
+								
+								<!-- 	<input class="search" type="text" placeholder="Search Your Product"> -->
+							<form class="form-group" method="get"><input class="form-control" type="text" placeholder="Search Your Product"></form>
+							<div class="" style=""><i class="fa fa-shopping-bag" style="font-size: 18px; font-color:blue;"></i></div>
+							</div>
+					
+						
+					</div>
+					</div>
+					
+					
+					
+			</div>
+		</nav>
+	</header>
+
+</body>
+</html>
